9c52d5d27474b1452684ad7c1e10150bcd630e42
Daniel Lundby <daniellundby@Daniels-MacBook-Pro.local>
2017-08-10 13:40:13 -0600

    making shit happen

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+
+var htmlRouter = require('./app/routes/htmlRoutes')
 // create out express instance
 var app = express();
 
 // setting the port
 
 var port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'app/public')));
+app.use('/', htmlRouter);
 
 app.listen(port, function(){
   console.log('server started on port:' + port);
